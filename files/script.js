07fe4ea79b3cf11a480d2bc6a8361d38a114725f
Ancorale <ancorale@ancorale.com>
2018-06-28 20:07:11 -0700

    6.28.18..2007

--- files/script.js
+++ files/script.js
@@ -1,10 +1,13 @@
 
 $(document).ready(function() {
   const presConst = {
-    masStr: 80, smBorHan: 50, fogNoz: 100, sprink: 150, stanPipWo: 200, relPum: 100, aptLoad: 140, highRise: 360};
-
-  const flConst ={}
-  let total = 0;
+    masStr: 80, smBorHan: 50, fogNoz: 100, sprink: 150, stanPipWo: 200, relPum: 100, aptLoad: 140, highRise: 360},
+   flConst = {
+    elup: 5, elDn: -5, appl: 10, stanPip: 25, monDek: 25, bliz: 20},
+   Hose = {
+    one34: 12, two12: 2, five: .08};
+    let drpDnItmVal = [];
+   let total = 0;
   $('input').on('change', function() {
     let number = $(this).val();
     total = +total + +number;
@@ -17,7 +20,11 @@ $(document).ready(function() {
   });
 
   $('.dropdown-item').on('click', function() {
-    let drpDnItm = $(this).prop('id');
+    let drpDnItm = $(this).prop('id'),
+    thisDrpDnItmVal = $(this).html();
+    drpDnItmVal.push(thisDrpDnItmVal  + "<br>");
+    $('#result').html(drpDnItmVal);
+    console.log(drpDnItmVal);
     console.log(drpDnItm);
   });
 
