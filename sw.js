d078caa53c49370322beddafe4924e1f4755254b
Ancorale <ancorale@ancorale.com>
2019-04-01 09:40:09 -0700

    4.1.19..940

--- sw.js
+++ sw.js
@@ -1,31 +1,32 @@
-  
-  if ('serviceWorker' in navigator) {
-    window.addEventListener('load', function () {
-      navigator.serviceWorker.register('/sw.js').then(function (registration) {
+  // pumpchart service worker
+
+  if (`serviceWorker` in navigator) {
+    window.addEventListener(`load`, function () {
+      navigator.serviceWorker.register(`/sw.js`).then(function (registration) {
         // Registration was successful
-        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        console.log(`ServiceWorker registration successful with scope: `, registration.scope);
       }, function (err) {
         // registration failed :(
-        console.log('ServiceWorker registration failed: ', err);
+        console.log(`ServiceWorker registration failed: `, err);
       });
     });
   }
 
 
-  var CACHE_NAME = 'ccfdpc-cache-v1';
+  var CACHE_NAME = `ccfdpc-cache-v1.1`;
   var urlsToCache = [
-    '/',
-    '/files/styles.css',
-    '/files/grid.css',
-    '/files/script.js'
+    `/`,
+    `/files/styles.css`,
+    `/files/grid.css`,
+    `/files/script.js`
   ];
 
-  self.addEventListener('install', function (event) {
+  self.addEventListener(`install`, function (event) {
     // Perform install steps
     event.waitUntil(
       caches.open(CACHE_NAME)
         .then(function (cache) {
-          console.log('Opened cache');
+          console.log(`Opened cache`);
           return cache.addAll(urlsToCache);
         })
     );
@@ -33,7 +34,7 @@
 
 
 
-self.addEventListener('fetch', function (event) {
+self.addEventListener(`fetch`, function (event) {
   event.respondWith(
     caches.match(event.request)
       .then(function (response) {
@@ -41,37 +42,53 @@ self.addEventListener('fetch', function (event) {
         if (response) {
           return response;
         }
-        return fetch(event.request);
-      }
-      )
+
+        // IMPORTANT: Clone the request. A request is a stream and
+        // can only be consumed once. Since we are consuming this
+        // once by cache and once by the browser for fetch, we need
+        // to clone the response.
+        var fetchRequest = event.request.clone();
+
+        return fetch(fetchRequest).then(
+          function (response) {
+            // Check if we received a valid response
+            if (!response || response.status !== 200 || response.type !== `basic`) {
+              return response;
+            }
+
+            // IMPORTANT: Clone the response. A response is a stream
+            // and because we want the browser to consume the response
+            // as well as the cache consuming the response, we need
+            // to clone it so we have two streams.
+            var responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then(function (cache) {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          }
+        );
+      })
   );
 });
 
 
-let deferredPrompt;
+self.addEventListener(`activate`, function (event) {
 
-window.addEventListener("beforeinstallprompt", e => {
-  // Prevent Chrome 67 and earlier from automatically showing the prompt
-  e.preventDefault();
-  // Stash the event so it can be triggered later.
-  deferredPrompt = e;
+  var cacheWhitelist = [`ccfdpc-cache-v1.1`];
 
-  btnAdd.addEventListener("click", e => {
-    // hide our user interface that shows our A2HS button
-    btnAdd.style.display = "none";
-    // Show the prompt
-    deferredPrompt.prompt();
-    // Wait for the user to respond to the prompt
-    deferredPrompt.userChoice.then(choiceResult => {
-      if (choiceResult.outcome === "accepted") {
-        console.log("User accepted the A2HS prompt");
-      } else {
-        console.log("User dismissed the A2HS prompt");
-      }
-      deferredPrompt = null;
-    });
-  });
-
-
+  event.waitUntil(
+    caches.keys().then(function (cacheNames) {
+      return Promise.all(
+        cacheNames.map(function (cacheName) {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
 });
 
